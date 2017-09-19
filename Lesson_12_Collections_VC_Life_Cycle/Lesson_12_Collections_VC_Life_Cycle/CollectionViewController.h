9af47e3666308f3bb2a63396e272aa261ec006fd
Andrey Proskurin <andrewproskurin@gmail.com>
2017-09-19 21:40:06 +0300

    UICollectionViewController

---
+++ Lesson_12_Collections_VC_Life_Cycle/Lesson_12_Collections_VC_Life_Cycle/CollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.h
+//  Lesson_12_Collections_VC_Life_Cycle
+//
+//  Created by Andrey Proskurin on 19.09.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewController : UICollectionViewController
+
+@end
